fc440dfbdb8ee8f5aab8f3aa27f63feef63ca4f1
prissmarquez <a01738866@tec.mx>
2025-08-26 20:05:58 -0600

    funciones de dropButton

--- lib/components/myDropDownButton1.dart
+++ lib/components/myDropDownButton1.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
-class Mydropdownbutton1 extends StatefulWidget {
-  const Mydropdownbutton1({super.key});
+class Mydropdownbutton1 <T>  extends StatefulWidget {
+  final List<T> items;
+  final T? selectedValue;
+  final ValueChanged<T?>? onChanged;
+  final String Function(T)? itemLabelBuilder;
+  final String? hinText;
+
+  const Mydropdownbutton1  ({
+    Key? key,
+    required this.items,
+    required this.selectedValue,
+    required this.onChanged,
+    required this.itemLabelBuilder,
+    required this.hinText
+    }) : super(key: key);
 
   @override
-  State<Mydropdownbutton1> createState() => _Mydropdownbutton1State();
+  State<Mydropdownbutton1 <T> > createState() => _Mydropdownbutton1 <T> (
+    
+  );
 }
 
-class _Mydropdownbutton1State extends State<Mydropdownbutton1> {
+class _Mydropdownbutton1 <T> extends State<Mydropdownbutton1 <T> > {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
