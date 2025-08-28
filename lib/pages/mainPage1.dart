927012c99a1d797329e865304d760005e6c4a998
prissmarquez <a01738866@tec.mx>
2025-08-28 10:17:37 -0600

    antes de widget

--- lib/pages/mainPage1.dart
+++ lib/pages/mainPage1.dart
@@ -19,240 +19,238 @@ class _MainpageState extends State<Mainpage1> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-        padding: EdgeInsets.only(top: 80),
-        child: Column(
-          children: [
-            Text(
-              "Calculadora de\n  Concreto",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: const Color.fromARGB(255, 10, 19, 106),
-              ),
-            ),
-
-            SizedBox(height: 30),
-
-            // Container(
-            //   child: Row(
-            //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-            //     children: [
-            //       Container(
-            //         width: 50,
-            //         height: 50,
-            //         decoration: BoxDecoration(
-            //           image: DecorationImage(
-            //             image: AssetImage("lib/images/cilindro.png"),
-            //           ),
-            //         ),
-            //       ),
-
-            //       Container(
-            //         width: 50,
-            //         height: 50,
-            //         decoration: BoxDecoration(
-            //           image: DecorationImage(
-            //             image: AssetImage("lib/images/cono.png"),
-            //           ),
-            //         ),
-            //       ),
-
-            //       Container(
-            //         width: 50,
-            //         height: 50,
-            //         decoration: BoxDecoration(
-            //           image: DecorationImage(
-            //             image: AssetImage("lib/images/invertir.png"),
-            //           ),
-            //         ),
-            //       ),
-            //     ]-,
-            //   ),
-            // ),
-
-            Container(
-              child: Column(
-                children: [
-                  Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Myselectableimage(
-                  imagePath: "lib/images/cilindro.png",
-                  //Tú pasas valores al widget MySelectableImage
-                  isSelected: selectedImage == "lib/images/cilindro.png",
-                  //es la función que se ejecuta cuando el usuario toca la imagen.
-                  //En este caso, cambia la variable selectedImage
-                  onTap: () {
-                    setState(() {
-                      selectedImage = "lib/images/cilindro.png";
-                    });
-                  },
-                ),
-            
-                Myselectableimage(
-                  imagePath: "lib/images/cono.png",
-                  isSelected: selectedImage == "lib/images/cono.png",
-                  onTap: () {
-                    setState(() {
-                      selectedImage = "lib/images/cono.png";
-                    });
-                  },
-                ),
-            
-                Myselectableimage(
-                  imagePath: "lib/images/invertir.png",
-                  isSelected: selectedImage == "lib/images/invertir.png",
-                  onTap: () {
-                    setState(() {
-                      selectedImage = "lib/images/invertir.png";
-                    });
-                  },
-                ),
-              ],
-            ),
-
-            SizedBox(height: 20),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  width: 80,
-                  height: 50,
-                  child: TextField(
-                    textAlignVertical: TextAlignVertical.center,
-                    keyboardType: TextInputType.number,
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter
-                          .digitsOnly, // Allow only digits
-                    ],
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintStyle: TextStyle(fontSize: 12),
-                      hintText: "Ancho",
-                    ),
-                  ),
-                ),
-                //para eligir la unidadd
-                Specialddownbutton(),
-              ],
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  width: 80,
-                  height: 50,
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter
-                          .digitsOnly, // Allow only digits
-                    ],
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintStyle: TextStyle(fontSize: 12),
-                      hintText: "Largo",
-                    ),
-                  ),
-                ),
-
-                //para eligir la unidadd
-                Specialddownbutton(),
-              ],
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //es el textField
-                SizedBox(
-                  width: 80,
-                  height: 50,
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter
-                          .digitsOnly, // Allow only digits
-                    ],
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintStyle: TextStyle(fontSize: 12),
-                      hintText: "Profundidad",
-                    ),
-                  ),
-                ),
-                //para eligir la unidadd
-                Specialddownbutton(),
-              ],
-            ),
-                ],
-              ),
-            ),
-            
-
-            SizedBox(height: 12),
-
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 12,
-                  vertical: 8,
-                ), // reduce espacio interno
-                minimumSize: Size(50, 30), // tamaño mínimo del botón
-              ),
-              onPressed: () {},
-              child: Text("Añade otra forma"),
-            ),
-
-            SizedBox(
-              height: 12,
-            ),
-
-            Text("Volumen en metros cúbicos (m³): "),
-
-            SizedBox(
-              width: 170,
-              child: TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly, // Allow only digits
-                ],
+          padding: EdgeInsets.only(top: 80),
+          child: Column(
+            children: [
+              Text(
+                "Calculadora de\n  Concreto",
                 textAlign: TextAlign.center,
-                decoration: InputDecoration(hintText: "m³"),
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 10, 19, 106),
+                ),
               ),
-            ),
 
-            Text("Porcentaje de reserva en (m³): "),
+              SizedBox(height: 30),
 
-            SizedBox(
-              width: 170,
-              child: TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly, // Allow only digits
-                ],
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(hintText: "m³"),
+              // Container(
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //     children: [
+              //       Container(
+              //         width: 50,
+              //         height: 50,
+              //         decoration: BoxDecoration(
+              //           image: DecorationImage(
+              //             image: AssetImage("lib/images/cilindro.png"),
+              //           ),
+              //         ),
+              //       ),
+
+              //       Container(
+              //         width: 50,
+              //         height: 50,
+              //         decoration: BoxDecoration(
+              //           image: DecorationImage(
+              //             image: AssetImage("lib/images/cono.png"),
+              //           ),
+              //         ),
+              //       ),
+
+              //       Container(
+              //         width: 50,
+              //         height: 50,
+              //         decoration: BoxDecoration(
+              //           image: DecorationImage(
+              //             image: AssetImage("lib/images/invertir.png"),
+              //           ),
+              //         ),
+              //       ),
+              //     ]-,
+              //   ),
+              // ),
+              Container(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Myselectableimage(
+                          imagePath: "lib/images/cilindro.png",
+                          //Tú pasas valores al widget MySelectableImage
+                          isSelected:
+                              selectedImage == "lib/images/cilindro.png",
+                          //es la función que se ejecuta cuando el usuario toca la imagen.
+                          //En este caso, cambia la variable selectedImage
+                          onTap: () {
+                            setState(() {
+                              selectedImage = "lib/images/cilindro.png";
+                            });
+                          },
+                        ),
+
+                        Myselectableimage(
+                          imagePath: "lib/images/cono.png",
+                          isSelected: selectedImage == "lib/images/cono.png",
+                          onTap: () {
+                            setState(() {
+                              selectedImage = "lib/images/cono.png";
+                            });
+                          },
+                        ),
+
+                        Myselectableimage(
+                          imagePath: "lib/images/invertir.png",
+                          isSelected:
+                              selectedImage == "lib/images/invertir.png",
+                          onTap: () {
+                            setState(() {
+                              selectedImage = "lib/images/invertir.png";
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+
+                    SizedBox(height: 20),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: 80,
+                          height: 50,
+                          child: TextField(
+                            textAlignVertical: TextAlignVertical.center,
+                            keyboardType: TextInputType.number,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter
+                                  .digitsOnly, // Allow only digits
+                            ],
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(fontSize: 12),
+                              hintText: "Ancho",
+                            ),
+                          ),
+                        ),
+                        //para eligir la unidadd
+                        Specialddownbutton(),
+                      ],
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: 80,
+                          height: 50,
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter
+                                  .digitsOnly, // Allow only digits
+                            ],
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(fontSize: 12),
+                              hintText: "Largo",
+                            ),
+                          ),
+                        ),
+
+                        //para eligir la unidadd
+                        Specialddownbutton(),
+                      ],
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        //es el textField
+                        SizedBox(
+                          width: 80,
+                          height: 50,
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter
+                                  .digitsOnly, // Allow only digits
+                            ],
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(fontSize: 12),
+                              hintText: "Profundidad",
+                            ),
+                          ),
+                        ),
+                        //para eligir la unidadd
+                        Specialddownbutton(),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 25),
 
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Typepage()),
-                );
-              },
-              child: Text("Siguiente"),
-            ),
-          ],
+              SizedBox(height: 12),
+
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 8,
+                  ), // reduce espacio interno
+                  minimumSize: Size(50, 30), // tamaño mínimo del botón
+                ),
+                onPressed: () {},
+                child: Text("Añade otra forma"),
+              ),
+
+              SizedBox(height: 12),
+
+              Text("Volumen en metros cúbicos (m³): "),
+
+              SizedBox(
+                width: 170,
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly, // Allow only digits
+                  ],
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(hintText: "m³"),
+                ),
+              ),
+
+              Text("Porcentaje de reserva en (m³): "),
+
+              SizedBox(
+                width: 170,
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly, // Allow only digits
+                  ],
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(hintText: "m³"),
+                ),
+              ),
+              SizedBox(height: 25),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Typepage()),
+                  );
+                },
+                child: Text("Siguiente"),
+              ),
+            ],
+          ),
         ),
       ),
-      )
     );
   }
 }
--- lib/pages/typePage.dart
+++ lib/pages/typePage.dart
@@ -1,9 +1,7 @@
-
 import 'package:cement_app/components/myDropDownButton.dart';
 import 'package:flutter/material.dart';
 
 class Typepage extends StatefulWidget {
-  
   const Typepage({super.key});
 
   @override
@@ -11,21 +9,35 @@ class Typepage extends StatefulWidget {
 }
 
 class _TypepageState extends State<Typepage> {
-  final List <String> elementos = [
-    "Cimentación", "Piso", "Banqueta", "Muro", "Cisterna", "Pavimento", "Losa", "Talud" , "Zapata", "Columna", "Otro"
-    ];
-    final List <String> servicio = [
-    "Tiro directo" , "Bomba"
-    ];
-    final List <String> resistencia = [
-    "150 kg/cm2", "200 kg/cm2", "250 kg/cm2", "300 kg/cm2", "350 kg/cm2"
-    ];
-     final List <String> fraguado =[
-    "3 dias", "7 dias", "14 dias", "28 dias"
-    ];
-     final List <String> revenimiento =[
-    "10 cm", "12 cm", "4 cm", "28 cm", "28 cm"
-    ];
+  final List<String> elementos = [
+    "Cimentación",
+    "Piso",
+    "Banqueta",
+    "Muro",
+    "Cisterna",
+    "Pavimento",
+    "Losa",
+    "Talud",
+    "Zapata",
+    "Columna",
+    "Otro",
+  ];
+  final List<String> servicio = ["Tiro directo", "Bomba"];
+  final List<String> resistencia = [
+    "150 kg/cm2",
+    "200 kg/cm2",
+    "250 kg/cm2",
+    "300 kg/cm2",
+    "350 kg/cm2",
+  ];
+  final List<String> fraguado = ["3 dias", "7 dias", "14 dias", "28 dias"];
+  final List<String> revenimiento = [
+    "10 cm",
+    "12 cm",
+    "4 cm",
+    "28 cm",
+    "28 cm",
+  ];
   String? elementoSelecionado;
   String? servicioSelecionado;
   String? resistenciaSelecionado;
@@ -34,7 +46,6 @@ class _TypepageState extends State<Typepage> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       body: Padding(
         padding: EdgeInsetsGeometry.all(12),
@@ -42,75 +53,80 @@ class _TypepageState extends State<Typepage> {
           child: Column(
             children: [
               Padding(padding: EdgeInsetsGeometry.all(30)),
+          
               Text("Elemento de Construcción: "),
               Mydropdownbutton<String>(
                 hinText: "Selecione un Elemento",
-                items: elementos, 
-                selectedValue: elementoSelecionado, 
+                items: elementos,
+                selectedValue: elementoSelecionado,
                 onChanged: (nuevoElemento) {
                   setState(() {
                     elementoSelecionado = nuevoElemento;
                   });
-                }, 
-                itemLabelBuilder: (elementos) => elementos, 
-                ),
-
-                Text("Tipo de Servicio: "),
+                },
+                itemLabelBuilder: (elementos) => elementos,
+              ),
+          
+              Text("Tipo de Servicio: "),
               Mydropdownbutton<String>(
                 hinText: "Selecione un Elemento",
-                items: servicio, 
-                selectedValue: servicioSelecionado, 
+                items: servicio,
+                selectedValue: servicioSelecionado,
                 onChanged: (nuevoServicio) {
                   setState(() {
                     servicioSelecionado = nuevoServicio;
                   });
-                }, 
-                itemLabelBuilder: (servicio) => servicio, 
-                ),
-
-
-                Text("Resistencia a la compresión (fc): "),
+                },
+                itemLabelBuilder: (servicio) => servicio,
+              ),
+          
+              Text("Resistencia a la compresión (fc): "),
               Mydropdownbutton<String>(
                 hinText: "Selecione un Elemento",
-                items: resistencia, 
-                selectedValue: resistenciaSelecionado, 
+                items: resistencia,
+                selectedValue: resistenciaSelecionado,
                 onChanged: (nuevaResistencia) {
                   setState(() {
                     resistenciaSelecionado = nuevaResistencia;
                   });
-                }, 
-                itemLabelBuilder: (resistencia) => resistencia, 
-                ),
-
-                Text("Tiempo de fraguado: "),
+                },
+                itemLabelBuilder: (resistencia) => resistencia,
+              ),
+          
+              Text("Tiempo de fraguado: "),
               Mydropdownbutton<String>(
                 hinText: "Selecione un Elemento",
-                items: fraguado, 
-                selectedValue: fraguadoSelecionado, 
+                items: fraguado,
+                selectedValue: fraguadoSelecionado,
                 onChanged: (nuevaFraguado) {
                   setState(() {
                     fraguadoSelecionado = nuevaFraguado;
                   });
-                }, 
-                itemLabelBuilder: (fraguado) => fraguado, 
-                ),
-
-                Text("Revenimiento en Centímetros (cm): "),
+                },
+                itemLabelBuilder: (fraguado) => fraguado,
+              ),
+          
+              Text("Revenimiento en Centímetros (cm): "),
               Mydropdownbutton<String>(
                 hinText: "Selecione un Elemento",
-                items: revenimiento, 
-                selectedValue: revenimientoSelecionado, 
+                items: revenimiento,
+                selectedValue: revenimientoSelecionado,
                 onChanged: (nuevoRevenimiento) {
                   setState(() {
                     revenimientoSelecionado = nuevoRevenimiento;
                   });
-                }, 
-                itemLabelBuilder: (revenimiento) => revenimiento, 
-                )
+                },
+                itemLabelBuilder: (revenimiento) => revenimiento,
+              ),
+          
+              SizedBox(height: 12),
+          
+              ElevatedButton(onPressed: () {}, child: Text("Siguiente")),
+              ElevatedButton(onPressed: () {}, child: Text("Atras")),
             ],
           ),
-        ) ,
-        )
+        ),
+      ),
     );
   }
 }
